9b892c6a02a3069ef048ca998af29922a68dde93
Rangerson Clemente <rangerson14@outlook.com>
2023-05-12 11:13:28 -0300

    Update pagina web

---
+++ script.js
@@ -0,0 +1,30 @@
+var caminho = "images/tela/";
+var Imagens = ["Login.png","principal.png", "funcionario_teste.png"];
+var NomeImagens = ["Tela de login","Tela principal","Tela de busca de funcionario"];
+var index = 0;
+
+function redirecionamento(){
+    window.alert("Voce sera redirecionado a uma nova aba para realizar o contanto.");
+    window.open("https://luizoliveira134.wixsite.com/hotelogix/blank-2", "_blanck");
+}
+
+function anterior(){
+    index--;
+    if(index<0){
+        index = Imagens.length - 1;
+    }
+    updateImage();
+}
+
+function proximo(){
+    index++;
+    if(index >= Imagens.length){
+        index = 0;
+    }
+    updateImage();
+}
+
+function updateImage(){
+    document.getElementById("picture").src = (caminho+Imagens[index]);
+    document.getElementById("picture").alt = ""+(NomeImagens[index])
+}
